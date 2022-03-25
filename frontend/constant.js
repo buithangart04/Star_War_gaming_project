858d752be80ed5f0c14630af8aec54fac45b5cf2
art04 <thangbuiduc2000@gmail.com>
2022-03-25 19:53:55 +0700

    star war v2 test for deploy

---
+++ frontend/constant.js
@@ -0,0 +1,15 @@
+const SCREEN_WIDTH = 1500;
+const SCREEN_HEIGHT = 1000;
+const ROOM_WIDTH = 6000;
+const ROOM_HEIGHT = 5000;
+const FOOD_SIZE = 50;
+const pre_charater_lv1= './img/character/level1/';
+const pre_charater_lv2= './img/character/level2/';
+const pre_charater_lv3= './img/character/level3/';
+const pre_food = './img/food/';
+
+
+
+const pre_weapon_lv1= './img/weapon/level1/';
+const pre_weapon_lv2= './img/weapon/level2/';
+const pre_weapon_lv3= './img/weapon/level3/';
